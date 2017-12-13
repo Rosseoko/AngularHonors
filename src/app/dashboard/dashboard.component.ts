20b6b99a08e06f4daa9fb876481f9cf7565888c3
Rosseoko <mirosrs@gmail.com>
2017-12-12 19:11:53 -0600

    Dashboard updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,18 +10,6 @@ import { Streak } from '../shared/streak';
 export class DashboardComponent implements OnInit {
 
 
-  dishes: Streak[] = [
-    {
-      name:'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      day: 'mains',
-      },
-   {
-      name:'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      day: 'appetizer',
-  } ];
-
   constructor() { }
 
   ngOnInit() {
